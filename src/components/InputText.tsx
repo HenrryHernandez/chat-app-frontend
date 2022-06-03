01b6f8e1a0150dcac4a642f92307ce9e6398754d
Henrry Hernandez <jenrruhndz@gmail.com>
2022-06-03 04:23:54 +0200

    style: create layout and generate basic components

---
+++ src/components/InputText.tsx
@@ -0,0 +1,3 @@
+export const InputText = () => {
+  return <div>InputText</div>;
+};
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,3 +1,32 @@
+import { ChatCard } from "../components/ChatCard";
+import { InformationCard } from "../components/InformationCard";
+import { InputText } from "../components/InputText";
+
 export const Main = () => {
-  return <></>;
+  return (
+    <div className="main">
+      <div style={{ backgroundColor: "blue" }} className="main__information">
+        <InformationCard />
+      </div>
+      <div style={{ backgroundColor: "green" }} className="main__chats">
+        <ChatCard />
+        <ChatCard />
+        <ChatCard />
+        <ChatCard />
+      </div>
+      <div
+        style={{ backgroundColor: "yellow" }}
+        className="main__chat-information"
+      >
+        <InformationCard />
+      </div>
+      <div
+        style={{ backgroundColor: "orange" }}
+        className="main__chat-screen"
+      ></div>
+      <div style={{ backgroundColor: "pink" }} className="main__keyboard">
+        <InputText />
+      </div>
+    </div>
+  );
 };
