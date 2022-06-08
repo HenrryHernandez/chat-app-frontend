6a4151866669d61c2a2d9f17350dfa7487e8ff9c
Henrry Hernandez <jenrruhndz@gmail.com>
2022-06-08 20:40:38 +0200

    feat: logout

--- src/components/InformationCard.tsx
+++ src/components/InformationCard.tsx
@@ -1,3 +1,22 @@
+import { useDispatch } from "react-redux";
+
+import { logoutAction } from "../redux/states/auth";
+import { removeUserAction } from "../redux/states/user";
+
 export const InformationCard = () => {
-  return <div>InformationCard</div>;
+  const dispatch = useDispatch();
+
+  const logout = () => {
+    dispatch(logoutAction());
+    dispatch(removeUserAction());
+
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+  };
+
+  return (
+    <div>
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
 };
